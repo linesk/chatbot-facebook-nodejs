f938046ade1665e12e669fc6012cfd23a16c855a
Ratchapoom <Ratchapoom.k@kkumail.com>
2018-08-23 20:03:32 +0700

    Initial configuration

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     FB_PAGE_TOKEN: 'EAAF3rAHaf4MBAPy8Cxnx90SU2lhyW3sdMGc0TLC9W6A0JJeTikLOL76cvrQZAnVSbWtpoH66OhOU6uoROlqkVtqkIrITJciZBRqSeelt1Rn3z4ZBnXZBlyk5tWhIZCWyzj3bIVqvNjqZBSiQqtmyth0N2rbnB8smKADSUAY4iVf4iYXW2ZBT1lV',
     FB_VERIFY_TOKEN: 'ratchapoom_kittiwattanasarn_med_43',
     FB_APP_SECRET: '4e2f897d921613c8bec52aa9bf564531',
-    SERVER_URL: 'https://secure-hollows-65095.herokuapp.com/',
+    SERVER_URL: 'https://cryptic-shore-19627.herokuapp.com/',
     GOOGLE_PROJECT_ID: '',
     DF_LANGUAGE_CODE: '',
     GOOGLE_CLIENT_EMAIL: '',
